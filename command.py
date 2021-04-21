572c912025aa0a60d037b25d696f4b1dace9fb7a
fofo920828 <fofo920828@gml.com>
2021-04-21 10:34:09 +0800

    20210421 final

---
+++ command.py
@@ -0,0 +1,15 @@
+#download git
+#print(" ")
+#git
+#echo "# 20210421-python" >> README.md
+#git init
+#git add README.md
+#git config --global user.email "fofo920828@gml.com"
+#git config --global user.name "fofo920828"
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://github.com/fofo920828/20210421-python.git
+
+#git add .
+#git commit -m "xxxxxxxxx"
+#git push -u origin main
